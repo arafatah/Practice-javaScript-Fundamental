e9be6169b9477a59f60d54ec4736f0be36610df5
arafatah <arafathowlader890@gmail.com>
2024-12-30 11:11:51 +0600

    loop

--- script.js
+++ script.js
@@ -289,30 +289,29 @@ for (let exercise = 1; exercise < 4; exercise++){
     }
 }
 
-*/
 
 for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep}`);
+    console.log(`Lifting weights repetition ${rep}`);
 }
 
 let rep = 1;
 while (rep <= 10) {
-  console.log(`White: Lifting weights repetition ${rep}`);
-  rep++;
+    console.log(`White: Lifting weights repetition ${rep}`);
+    rep++;
 }
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 
 while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log("Loop is about to end...");
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log("Loop is about to end...");
 }
 
 // challenge 4
 
 const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
@@ -320,9 +319,9 @@ const tips = [];
 const totals = [];
 
 for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(bills[i] + tip);
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
 }
 
 console.log(bills, tips, totals)
@@ -339,10 +338,81 @@ console.log(calcAverage(totals));
 console.log(calcAverage(tips));
 
 
+const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+
+        if (typeof curTemp !== 'number') continue;
+
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+
+    console.log(max, min);
+    return max - min;
+};
 
 
+const amplitude = calcTempAmplitude(temperature);
+console.log(amplitude);
 
 
+const calcTempAmplitudeNew = function (t1, t2) {
+    const temps = t1.concat(t2);
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+
+        if (typeof curTemp !== 'number') continue;
+
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+
+    console.log(max, min);
+    return max - min;
+};  
+
+const calcTempAmplitudeNew = function(t1, t2){ 
+    const temps = t1.concat(t2);
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++){
+        const curTemp = temps[i];
+
+        if (typeof curTemp !== 'number') continue;
+
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}
+
+const amplitude = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitude);
+
+*/
 
 
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
+const printForecast = function (arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++){
+        str += `${arr[i]}°C in ${i + 1} days...`;
+    }
+    console.log('...' + str);
+}
+
+printForecast(data1);
+printForecast(data2);
