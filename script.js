c036ea88609f318f4de62e150567748b62dea996
arafatah <arafathowlader890@gmail.com>
2024-12-31 12:42:15 +0600

    last commit

--- script.js
+++ script.js
@@ -477,3 +477,37 @@ const calcAverage = function (arr) {
 console.log(calcAverage([3, 4, 4]));
 console.log(calcAverage(totals));
 console.log(calcAverage(tips));
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitude);
+
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    return this.length > 0 ? this[this.length - 1] : -1
+ };
+ 
+ /**
+  * const arr = [1, 2, 3];
+  * arr.last(); // 3
+  */
